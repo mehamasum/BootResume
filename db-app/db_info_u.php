ba8a8c81c15dcd80ab26008f77507c4a986d49de
mehamasum <mehamasum@gmail.com>
2017-04-25 00:15:59 +0600

    init

--- db-app/db_info_u.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// db login variables
-	$dbhost = 'br.nealshyam.com';
-	$dbname = 'bootresume';
-	$dbuser = 'bootresupdate';
-	$dbpass = 'h5P1235k3Uhg';
-?>
--- db-app/db_info_v.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// db login variables
-	$dbhost = 'br.nealshyam.com';
-	$dbname = 'bootresume';
-	$dbuser = 'bootresview';
-	$dbpass = 'h5PHI3x567x1';
-?>
