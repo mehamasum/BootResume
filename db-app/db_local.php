10474ee81f1770cc1751c5fcc4eb57dcd074396c
mehamasum <mehamasum@gmail.com>
2017-04-26 15:27:41 +0600

    pp circle

---
+++ db-app/db_local.php
@@ -0,0 +1,17 @@
+<?php
+$a_host= 'localhost';
+$a_user= 'root';
+$a_pass= '';
+$a_db= 'elegantres';
+
+// Create connection
+$conn = mysqli_connect($a_host, $a_user, $a_pass, $a_db);
+// Check connection
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+?>
