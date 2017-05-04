763c70e10181a551dec0df7b48b2d5de2f448025
mehamasum <mehamasum@gmail.com>
2017-05-04 21:07:56 +0600

    azure redirection test

--- dashboard/includes/menu.php
+++ dashboard/includes/menu.php
@@ -2,7 +2,7 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
         <div class="navbar nav_title" style="border: 0;">
-            <a href="index.html" class="site_title"><i class="fa fa-asterisk"></i> <span>Elegant Resume</span></a>
+            <a href="index.php" class="site_title"><i class="fa fa-asterisk"></i> <span>Elegant Resume</span></a>
         </div>
 
         <div class="clearfix"></div>
